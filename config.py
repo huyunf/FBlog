f51c7d0592388e889c16cde942fe996da30798e3
Yunfeng Hu <huyunf@marvell.com>
2016-04-29 17:29:19 -0700

    add config file

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+basdir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY  = os.envrion.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    FLASKY_MAIL_SUBJECT_PREFIX  = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <yfhu.dev@hotmail.com>'
+    FLASKY_ADMIN = os.envrion.get('FLASKY_ADMIN')
+    
+
